4484aa361e259752e3523d554e49b8366bcb6d19
Денис Болгов <55257908+bolgovsky@users.noreply.github.com>
2022-10-11 11:29:16 +0300

    Update RDP_patch.ps1
    add more codes for different OS

--- Desktop/RDP_patch.ps1
+++ Desktop/RDP_patch.ps1
@@ -1,6 +1,22 @@
 # PowerShell скрипт модифицирует файл termsrv.dll, разрешая множественные RDP подключения к рабочим станциям на базе Windows 10 (1809 и выше) и Windows 11
 # Подробности https://winitpro.ru/index.php/2015/09/02/neskolko-rdp-sessij-v-windows-10/
 
+<# what to find 
+Windows 11 RTM ( 21H2 22000.258)	39 81 3C 06 00 00 0F 84 4F 68 01 00	 
+Windows 10 x64 21H2	39 81 3C 06 00 00 0F 84 DB 61 01 00
+Windows 10 x64 21H1	39 81 3C 06 00 00 0F 84 2B 5F 01 00
+Windows 10 x64 20H2	39 81 3C 06 00 00 0F 84 21 68 01 00
+Windows 10 x64 2004	39 81 3C 06 00 00 0F 84 D9 51 01 00
+Windows 10 x64 1909	39 81 3C 06 00 00 0F 84 5D 61 01 00
+Windows 10 x64 1903	39 81 3C 06 00 00 0F 84 5D 61 01 00
+Windows 10 x64 1809	39 81 3C 06 00 00 0F 84 3B 2B 01 00
+Windows 10 x64 1803	8B 99 3C 06 00 00 8B B9 38 06 00 00
+Windows 10 x64 1709	39 81 3C 06 00 00 0F 84 B1 7D 02 00
+#> 
+
+<# replace to 
+B8 00 01 00 00 89 81 38 06 00 00 90 
+#>
 
 # Остановить службу, сделать копию файл и изменить разрешения
 Stop-Service UmRdpService -Force
